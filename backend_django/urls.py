3784c7792c37d6dc3eb9d41ed64d6a926c5c4e6a
LocPT1602 <133625168+LocPT1602@users.noreply.github.com>
2025-05-03 17:12:48 +0700

    update playlist update

--- backend_django/urls.py
+++ backend_django/urls.py
@@ -83,7 +83,7 @@ api_urlpatterns  = [
     path('playlist', playlistView.get_playlist_by_clerki_id, name='get_playlist_by_id'),
     path('playlist/<str:playlist_id>', playlistView.get_playlist_by_id, name='get_playlist_by_id'),
     path('playlists/create', playlistView.create_playlist, name='create_playlist'),
-    path('playlists/<str:playlist_id>', playlistView.update_playlist, name='update_playlist'),
+    path('playlists/<str:playlist_id>/update', playlistView.update_playlist, name='update_playlist'),
     path('playlists/<str:playlist_id>/add-song', playlistView.add_song_to_playlist, name='add_song_to_playlist'),
 
     
